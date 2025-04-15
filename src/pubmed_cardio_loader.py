932c9287c778d83be22bc8a64b161342db8729d2
hasnat79 <hasnatabdullah79@gmail.com>
2025-04-15 11:44:37 -0500

    pubmed cardio dataset loader

---
+++ src/pubmed_cardio_loader.py
@@ -0,0 +1,31 @@
+from datasets import load_dataset
+
+
+class PubMedCardioLoader:
+    def __init__(self, dataset_name: str):
+        self.dataset = load_dataset(dataset_name)
+        print(self.dataset)
+
+    def __iter__(self):
+        """Returns an iterator over the dataset."""
+        for split in self.dataset:
+            for item in self.dataset[split]:
+                yield item
+    
+
+
+if __name__ == "__main__":
+    dataset_name = "hasnat79/pubmed25_cardio"
+    data_path = "/scratch/group/instalearn/InstaLearn/data/pubmed25_cardio.csv"
+
+    pubmed_cardio_loader = PubMedCardioLoader(dataset_name)
+    for item in pubmed_cardio_loader:
+        print(item["PMID"])
+        print(item["Abstract"])
+
+#     DatasetDict({
+#     train: Dataset({
+#         features: ['PMID', 'PMID_Version', 'Title', 'Status', 'IndexingMethod', 'Owner', 'Abstract', 'Journal_Title', 'ISSN', 'ISSN_Type', 'Volume', 'Issue', 'CitedMedium', 'PubDate_Year', 'PubDate_Month', 'PubDate_Day', 'Pagination', 'Authors', 'PublicationTypes', 'Chemicals', 'MeSHHeadings_Full', 'MeSH_Descriptors', 'MeSH_Qualifiers', 'MeSH_MajorTopics', 'Grants', 'DateCompleted', 'DateRevised', 'PublicationHistory', 'ArticleIDs', 'category', 'sub_category'],
+#         num_rows: 864244
+#     })
+# })
